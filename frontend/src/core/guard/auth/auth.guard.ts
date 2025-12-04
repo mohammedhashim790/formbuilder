74b7afadf9c66f136b0ff11600cf23e56b10c435
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-12-04 18:14:37 -0400

    feat: application interceptor and guard

---
+++ frontend/src/core/guard/auth/auth.guard.ts
@@ -0,0 +1,12 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const {requiresAuth} = route.data;
+  if (requiresAuth ?? false) {
+    // TODO, check if user is authorised, if not redirect to sign in
+    // router.navigate(['/login']);
+  }
+  return true;
+};
