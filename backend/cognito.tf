5c342e213f3d3705c096f7674c3beed98beb9fea
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-12-11 15:49:27 -0400

    feat(terraform): Cognito Setup with basic User Password Auth Flow.

--- backend/cognito.tf
+++ backend/cognito.tf
@@ -1,14 +1,23 @@
-# resource "aws_cognito_user_pool" "main" {
-#   name = "${var.project_name}-user-pool"
-#
-#   auto_verified_attributes = ["email"]
-#
-#   password_policy {
-#     minimum_length    = 8
-#     require_lowercase = true
-#     require_numbers   = true
-#     require_symbols   = false
-#     require_uppercase = true
-#   }
-# }
-#
+resource "aws_cognito_user_pool" "cognito" {
+  name = "${var.project_name}-user-pool"
+
+  auto_verified_attributes = ["email"]
+
+
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = false
+    require_uppercase = true
+  }
+}
+
+
+resource "aws_cognito_user_pool_client" "client_app" {
+  name                = "${var.project_name}-user-pool"
+  user_pool_id        = aws_cognito_user_pool.cognito.id
+  explicit_auth_flows = ["USER_PASSWORD_AUTH"]
+}
+
