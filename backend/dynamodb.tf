43dc20c419a26bf688a61153aabcd8fe2cf39094
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-12-10 21:23:58 -0400

    feat(terraform): DynamoDB Setup

---
+++ backend/dynamodb.tf
@@ -0,0 +1,14 @@
+
+
+# Sample
+# resource "aws_dynamodb_table" "orders" {
+#   name         = "orders"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "PK"
+#   range_key    = "SK"
+#
+#   attribute { name = "PK", type = "S" }
+#   attribute { name = "SK",type = "S" }
+#
+#   tags = { Name = "${var.project_name}-orders-table" }
+# }
